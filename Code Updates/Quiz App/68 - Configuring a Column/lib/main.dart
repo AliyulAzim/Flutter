52d6acebf1bd0e0eb3962779874a3593c0002be1
Mohd. Aliyul Azim Khan <162032571+AliyulAzim@users.noreply.github.com>
2024-08-09 00:17:03 +0530

    Delete Code Updates/Quiz App/68 - Configuring a Column directory

--- Code Updates/Quiz App/68 - Configuring a Column/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:quiz_app/quiz.dart';
-
-
-void main() {
-  runApp(const Quiz());
-}
--- Code Updates/Quiz App/68 - Configuring a Column/lib/quiz_questions.dart
+++
@@ -1,7 +0,0 @@
-
-class QuizQuestion {
-  const QuizQuestion(this.text, this.answers);
-
-  final String text;
-  final List<String> answers;
-}
